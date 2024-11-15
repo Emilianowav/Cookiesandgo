214eb0ad8c44ef14bd7cac3a86724dd554eb9f6a
Emiliano <emidbiasi@gmail.com>
2024-11-15 20:18:08 -0300

    Home 90%

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 // src/app/seccion1/page.tsx
 import Image from 'next/image';
 import styles from './home.module.css';
@@ -8,17 +9,35 @@ import arreglo from "../../../public/images/arregloHome.svg"
 import tortuga from "../../../public/images/tortuga.png"
 import planta from "../../../public/images/planta.png"
 import galleta from "../../../public/images/galletahome.png"
+import { useState } from 'react';
 
 export default function Seccion1() {
+
+    const [menuVisible, setMenuVisible] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    };
+
   return (
     <div className={styles.body}>
         <div className={styles.headerContainer}>
             <div >
                 <Image src={logo}alt="Next.js Logo" width={220} height={38} />
+            </div> 
+            <div className={styles.menuButton} onClick={toggleMenu}>
+          <Image src={menu} alt="Menu Icon" width={40} height={40} />
             </div>
-            <div>
-                <Image src={menu}alt="Next.js Logo" width={32} height={32} />
-            </div>
+        </div>
+
+        {/* Menú Desplegable */}
+        <div className={`${styles.menu} ${menuVisible ? styles.menuVisible : ''}`}>
+            <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Contacto</a></li>
+            </ul>
         </div>
         <div className={styles.content}>
             <div className={styles.textcontent}>
@@ -29,22 +48,26 @@ export default function Seccion1() {
                 </p>
                 <a className={styles.button} href="#">Nuestros Productos</a>
             </div>
-            <div className={styles.videocontent}>
-                <video className={styles.video} controls autoPlay loop muted>
-                    <source src="/videos/videoHome.mp4" type="video/mp4" />
-                </video>
-                
-                <div className={styles.galleta}>
-                    <Image src={galleta}alt="Next.js Logo" width={200} height={200} />
+            <div className={styles.productcontent}>
+                <div className={styles.videocontent}>
+                    <video className={styles.video} autoPlay loop muted>
+                        <source src="/videos/videoHome.mp4" type="video/mp4" />
+                    </video>
+                    
+                    <div className={styles.galleta}>
+                        <Image src={galleta}alt="Next.js Logo" width={200} height={200} />
+                    </div>
+                    <h2 className={styles.subtitle}>Gran Tortuga</h2>
                 </div>
-                <h2 className={styles.subtitle}>Gran Tortuga</h2>
+                <p className={styles.producttext}>Mas de 15g de proteina! 💪🤯 <br /> <a className={styles.detallelink} href="#">Detalle nutricional</a> </p>
             </div>
+            
         </div>
         <div className={styles.arreglo}>
             <Image src={arreglo}alt="Next.js Logo" width={700} height={700} />
         </div>
         <div className={styles.tortuga}>
-            <Image src={tortuga}alt="Next.js Logo" width={500} height={700} />
+            <Image src={tortuga}alt="Next.js Logo" width={475} height={700} />
         </div>
         <div className={styles.planta}>
             <Image src={planta}alt="Next.js Logo" width={200} height={200} />
